821589d29fac0751a70334450984dce16b8a3036
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-02-15 01:37:34 +0530

    Add files via upload

---
+++ WAP TO PRINT NaN.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	System.out.println(0.0F/0.0F);
+	System.out.println(Math.sqrt(-25));
+	}
+}
+
+//Negative values are store in the memory by the 2’s complement.
+//left shift it operates fatest multiplication.
+//Right Shift it operates the Fatest Division.
+//we can swap the elements without using extra element .
+  Ex=a=a^b;
+	b=a^b;
+	a=a^b; 
+
+
+
---
+++ WAP to print first 25 letters of telugu.java
@@ -0,0 +1,10 @@
+//printing the first 25 telugu words
+
+public class Main
+{
+	public static void main(String[] args) {
+	    char ch='\u0c05';//0x0c05,3077
+	    for(int i=1;i<25;i++)
+		System.out.println(ch++ +"  ");
+	}
+}
